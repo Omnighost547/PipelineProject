26afb27537e0bf8ad4fef26bd4ec5aeda082a5ae
G00347737@gmit.ie <Zq3uVF$8>
2020-05-10 22:00:26 +0100

    Fixx data breach

--- src/test/java/Test_Contact.java
+++ src/test/java/Test_Contact.java
@@ -37,53 +37,40 @@ public class Test_Contact {
         assertEquals("Kelly",myContact.getSecondName());
         assertEquals("12345678",myContact.getPhoneNr());
     }
+
     @Test
     void testFullConstructor(){
-        Contact myContact = new Contact("Sean","Kelly","Monivea","Athenry","Galway","12345678","15/05/1998","H65HN81");
+        Contact myContact = new Contact("Sean","Kelly","Galway","Galway","Galway","12345678","10/06/1999","8fuxkoj");
         assertEquals("Sean",myContact.getName());
         assertEquals("Kelly",myContact.getSecondName());
         assertEquals("12345678",myContact.getPhoneNr());
-        assertEquals("Monivea",myContact.getAddressLine1());
-        assertEquals("Athenry",myContact.getAddressLine2());
+        assertEquals("Galway",myContact.getAddressLine1());
+        assertEquals("Galway",myContact.getAddressLine2());
         assertEquals("Galway",myContact.getCity());
-        assertEquals("15/05/1998",myContact.getDOB());
-        assertEquals("H65HN81",myContact.getEirCode());
-
+        assertEquals("10/06/1999",myContact.getDOB());
+        assertEquals("8fuxkoj",myContact.getEirCode());
     }
+
     @Test
     void testFullDestructor(){
         Contact myContact = new Contact(" "," "," "," "," "," "," "," ");
         myContact.setName("Sean");
         myContact.setSecondName("Kelly");
         myContact.setPhoneNr("12345678");
-        myContact.setAddressLine1("Monivea");
-        myContact.setAddressLine2("Athenry");
+        myContact.setAddressLine1("Galway");
+        myContact.setAddressLine2("Galway");
         myContact.setCity("Galway");
-        myContact.setDOB("15/05/1998");
-        myContact.setEirCode("H65HN81");
+        myContact.setDOB("10/06/1999");
+        myContact.setEirCode("8fuxkoj");
         assertEquals("Sean",myContact.getName());
         assertEquals("Kelly",myContact.getSecondName());
         assertEquals("12345678",myContact.getPhoneNr());
-        assertEquals("Monivea",myContact.getAddressLine1());
-        assertEquals("Athenry",myContact.getAddressLine2());
+        assertEquals("Galway",myContact.getAddressLine1());
+        assertEquals("Galway",myContact.getAddressLine2());
         assertEquals("Galway",myContact.getCity());
-        assertEquals("15/05/1998",myContact.getDOB());
-        assertEquals("H65HN81",myContact.getEirCode());
+        assertEquals("10/06/1999",myContact.getDOB());
+        assertEquals("8fuxkoj",myContact.getEirCode());
 
     }
-    @Test
-    void testName(){
-     // Name myName = new Name("Sean") ;
-    }
 
-
-
-    @Test
-    void testGetters(){
-
-    }
-    @Test
-    void testSetters(){
-
-    }
 }
