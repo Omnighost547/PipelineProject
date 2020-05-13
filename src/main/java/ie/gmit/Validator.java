456a828344f6b912e498e40b7e6a034aadf491ab
theozas <djozas@gmail.com>
2020-05-13 11:47:08 +0100

    prechecked commit

--- src/main/java/ie/gmit/Validator.java
+++ src/main/java/ie/gmit/Validator.java
@@ -13,7 +13,8 @@ import java.util.regex.Matcher;
 public class Validator {
 
     //variables used in class
-    private int notification;
+    private int errorCode;//this will store error code from validation
+    //pattern variables will hold fixt patterns
     final private String namePattern = "^[A-Z][a-z]+";
     final private String phoneNumberPattern = "[0-9]+";
     final private String dobPattern = "[0-9]{2}/[0-9]{2}/[0-9]{4}";
@@ -29,54 +30,54 @@ public class Validator {
         pattern = Pattern.compile(namePattern);
         matcher = pattern.matcher(name);
         if(matcher.matches()){
-            notification = 0;
+            errorCode = 0;
         }else{
-            notification = 1;//
+            errorCode = 1;//
         }
-        return notification;
+        return errorCode;
     }
     //function to validate second name
     public int validateSecondName(String secondName){
         pattern = Pattern.compile(namePattern);
         matcher = pattern.matcher(secondName);
         if(matcher.matches()){
-            notification = 0;
+            errorCode = 0;
         }else{
-            notification = 2;
+            errorCode = 2;
         }
-        return notification;
+        return errorCode;
     }
     //function to validate phone number
     public int validatePhoneNumber(String phoneNumber){
         pattern = Pattern.compile(phoneNumberPattern);
         matcher = pattern.matcher(phoneNumber);
         if(matcher.matches()){
-            notification = 0;
+            errorCode = 0;
         }else{
-            notification = 3;
+            errorCode = 3;
         }
-        return notification;
+        return errorCode;
     }
     //function to validate date of birth
     public int validateDOB(String dob){
         pattern = Pattern.compile(dobPattern);
         matcher = pattern.matcher(dob);
         if(matcher.matches()){
-            notification = 0;
+            errorCode = 0;
         }else{
-            notification = 4;
+            errorCode = 4;
         }
-        return notification;
+        return errorCode;
     }
     //function to validate eirCode
     public int validateEirCode(String eirCode){
         pattern = Pattern.compile(eirCodePattern);
         matcher = pattern.matcher(eirCode);
         if(matcher.matches()){
-            notification = 0;
+            errorCode = 0;
         }else{
-            notification = 5;
+            errorCode = 5;
         }
-        return  notification;
+        return  errorCode;
     }
 }
--- src/test/java/ie/gmit/TestValidator.java
+++ src/test/java/ie/gmit/TestValidator.java
@@ -3,46 +3,63 @@ package ie.gmit;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class TestValidator {
-    Validator validator;
+    private Validator validator;
 
 
-    @DisplayName("Creating Validator object")
+
+
+    @DisplayName("  ******************************************\n" +
+                    "Test starting\n" +
+                    "******************************************")
     @BeforeEach
-    void createValidatorObject(){
+    void setup(){
         validator = new Validator();
+
+
     }
 
-    @DisplayName("Happy scenario testing for name validation")
+    @DisplayName(   "******************************************\n" +
+                    "Happy scenario testing for name validation\n" +
+                    "******************************************")
     @Test
     void testValidateName(){
        int response = validator.validateName("Paulius");
         assertEquals(0,response);
     }
 
-    @DisplayName("Happy scenario testing for second name validation")
+    @DisplayName(   "******************************************\n"+
+                    "Happy scenario testing for second name validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateSecondName(){
         int response = validator.validateSecondName("Miliunas");
         assertEquals(0,response);
     }
 
-    @DisplayName("Happy scenario testing for phone number validation")
+    @DisplayName(   "******************************************\n"+
+                    "Happy scenario testing for phone number validation\n"+
+                    "******************************************\n")
     @Test
     void testValidatePhoneNumber(){
         int response = validator.validatePhoneNumber("123456789");
         assertEquals(0,response);
     }
 
-    @DisplayName("Happy scenario testing for Date of birth validation")
+    @DisplayName(   "******************************************\n"+
+                    "Happy scenario testing for dob validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateDOB(){
         int response = validator.validateDOB("13/09/1987");
         assertEquals(0,response);
     }
 
-    @DisplayName("Happy scenario testing for Eir code validation")
+    @DisplayName(   "******************************************\n"+
+                    "Happy scenario testing for eircode validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateEirCode(){
         int response = validator.validateEirCode("H91 FRN2");
@@ -50,35 +67,46 @@ public class TestValidator {
     }
 
     //******************
-    @DisplayName("Fail testing for name validation")
+
+    @DisplayName(   "******************************************\n"+
+                    "Fail testing for name validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateNameFail(){
-        int response = validator.validateName("PAulius");
+        int response = validator.validateName("paulius");
         assertEquals(1,response);
     }
 
-    @DisplayName("Fail testing for second name validation")
+    @DisplayName("  ******************************************\n"+
+                    "Fail testing for second name validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateSecondNameFail(){
-        int response = validator.validateSecondName("MIliunas");
+        int response = validator.validateSecondName("miliunas");
         assertEquals(2,response);
     }
 
-    @DisplayName("Fail testing for phone number validation")
+    @DisplayName(   "******************************************\n"+
+                    "Fail testing for phone number validation\n"+
+                    "******************************************\n")
     @Test
     void testValidatePhoneNumberFail(){
         int response = validator.validatePhoneNumber("one two tree");
         assertEquals(3,response);
     }
 
-    @DisplayName("Fail testing for Date of birth validation")
+    @DisplayName(   "******************************************\n"+
+                    "Fail testing for Date of birth validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateDOBFail(){
         int response = validator.validateDOB("13091987");
         assertEquals(4,response);
     }
 
-    @DisplayName("Fail testing for Eir code validation")
+    @DisplayName(   "******************************************\n"+
+                    "Fail testing for Eir code validation\n"+
+                    "******************************************\n")
     @Test
     void testValidateEirCodeFail(){
         int response = validator.validateEirCode(" FRN2");
